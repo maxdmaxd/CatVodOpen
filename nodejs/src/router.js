035ef82c6f7040d5388611dc8833d193e4e3d091
hqkong <59519050+hqkong@users.noreply.github.com>
2024-04-29 15:32:55 +0800

    Update router.js

--- nodejs/src/router.js
+++ nodejs/src/router.js
@@ -24,9 +24,9 @@ import meijumi from './spider/video/meijumi.js';
 import vcm3u8 from './spider/video/vcm3u8.js';
 import baipiao from './spider/video/baipiaoys.js';
 import xxpan from './spider/video/xxpan.js';
-import ikan from './spider/video/ikanbot.js';
  
-const spiders = [douban,live,bili,baipiao,meijumi,ikan,wogg,xxpan,yingso,upyun,pansearch,push, alist, _13bqg,copymanga,laobaigs,bengou,baozimh,bookan];
+ 
+const spiders = [douban,live,bili,baipiao,meijumi,wogg,xxpan,yingso,upyun,pansearch,push, alist, _13bqg,copymanga,laobaigs,bengou,baozimh,bookan];
 const spiderPrefix = '/spider';
 
 /**
